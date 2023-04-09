ae0bf62830bb7d7b949bee67a544d8fb19e98889
Dogthemachine <danileyko@gmail.com>
2023-04-09 18:59:27 +0200

    First touch

--- Razom/bot/views.py
+++ Razom/bot/views.py
@@ -8,43 +8,13 @@ import environ
 import telebot
 from datetime import datetime
 
-
 env = environ.Env()
 environ.Env.read_env()
 telegram_bot = telebot.TeleBot(settings.TOKEN, threaded=False)
 
-# class BasicBotView(View):
-#     def get(self, request):
-#
-#             request,
-#             "page.html", {},
-#         )
-
-
-# @csrf_exempt
-# class BasicBotView(View):
-#     @bot.message_handler(commands=['start', 'help'])
-#     def send_welcome(message):
-#         bot.reply_to(message, "Слава Україні!")
-#
-#     @bot.message_handler(func=lambda message: True)
-#     def echo_all(message):
-#         bot.reply_to(message, message.text)
-#
-#     bot.infinity_polling()
 
 class BasicBotView(View):
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    with open(settings.MEDIA_ROOT + "/log.txt", 'w') as file:
-        file.write(f"{now}: In the BasicBotView but before post function\n")
-        file.write(settings.TOKEN)
-
     def post(self, request):
-
-        with open(settings.MEDIA_ROOT + "/log.txt", 'w') as file:
-            file.write("In the post function before: if request.method == POST\n")
-
         if request.method == "POST" and request.content_type == "application/json":
             try:
                 json_string = request.body.decode("utf-8")
@@ -53,19 +23,16 @@ class BasicBotView(View):
                 return HttpResponse(status=403)
             if update.message and update.message.text:
                 telegram_bot.process_new_messages([update.message])
-
-            with open(settings.MEDIA_ROOT + "/log.txt", 'w') as file:
-                file.write(f"{now}: Status=200\n")
-
             return HttpResponse(status=200)
         else:
-
-            with open(settings.MEDIA_ROOT + "/log.txt", 'w') as file:
-                file.write(f"{now}: status=403\n")
-
             return HttpResponse(status=403)
 
 
     @telegram_bot.message_handler(commands=["help", "start"])
     def telegram_welcome(message):
         telegram_bot.send_message(message.chat.id, "Слава Україні!")
+
+
+    @telegram_bot.message_handler(commands=["Героям слава!"])
+    def telegram_welcome(message):
+        telegram_bot.send_message(message.chat.id, "Русні пізда")

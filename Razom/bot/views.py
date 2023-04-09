3963dac4a6b4570ba8a7fefe956b7b4718e65d44
Dogthemachine <danileyko@gmail.com>
2023-04-09 15:11:08 +0200

    TelegramBot setting view

--- Razom/bot/views.py
+++ Razom/bot/views.py
@@ -9,25 +9,43 @@ import telebot
 
 env = environ.Env()
 environ.Env.read_env()
-bot = telebot.AsyncTeleBot(settings.TOKEN)
+telegram_bot = telebot.TeleBot(settings.TOKEN, threaded=False)
 
 # class BasicBotView(View):
 #     def get(self, request):
 #
-#         return render(
 #             request,
 #             "page.html", {},
 #         )
 
 
-@csrf_exempt
-class BasicBotView(View):
-    @bot.message_handler(commands=['start', 'help'])
-    def send_welcome(message):
-        bot.reply_to(message, "Слава Україні!")
+# @csrf_exempt
+# class BasicBotView(View):
+#     @bot.message_handler(commands=['start', 'help'])
+#     def send_welcome(message):
+#         bot.reply_to(message, "Слава Україні!")
+#
+#     @bot.message_handler(func=lambda message: True)
+#     def echo_all(message):
+#         bot.reply_to(message, message.text)
+#
+#     bot.infinity_polling()
 
-    @bot.message_handler(func=lambda message: True)
-    def echo_all(message):
-        bot.reply_to(message, message.text)
 
-    bot.infinity_polling()
+def BasicBotView(request):
+    if request.method == "POST" and request.content_type == "application/json":
+        try:
+            json_string = request.body.decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+        except:
+            return HttpResponse(status=403)
+        if update.message and update.message.text:
+            telegram_bot.process_new_messages([update.message])
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=403)
+
+
+@telegram_bot.message_handler(commands=["help", "start"])
+def telegram_welcome(message):
+    telegram_bot.send_message(message.chat.id, "Слава Україні!")

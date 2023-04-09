65322cdc814ce9fc009fae286a2700b6ef1a2495
Dogthemachine <danileyko@gmail.com>
2023-04-09 18:47:28 +0200

    Added some logging

--- Razom/bot/urls.py
+++ Razom/bot/urls.py
@@ -5,6 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 app_name = "bot"
 
 urlpatterns = [
-    path("", csrf_exempt(views.BasicBotView.as_view()), name="bot"),
+    path("", views.BasicBotView.as_view(), name="bot"),
     # path("", views.telegram_webhook, name="webhook"),
 ]

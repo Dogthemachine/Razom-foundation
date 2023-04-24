816e28ecce2b78032ab8882e576a2e8c4c83999a
Dogthemachine <danileyko@gmail.com>
2023-04-24 21:49:28 +0200

    Making dialog logic

--- Razom/bot/views.py
+++ Razom/bot/views.py
@@ -31,7 +31,7 @@ def BasicBotView(request):
 
         if update.callback_query:
 
-            bot.add_callback_query_handler([update.callback_query])
+            bot.process_new_messages([update.callback_query])
 
 
         return HttpResponse(status=200)

c342bab480db76254f3e9e29278ef7cfd27034a1
Dogthemachine <danileyko@gmail.com>
2023-04-25 08:27:19 +0200

    Making dialog logic

--- Razom/bot/views.py
+++ Razom/bot/views.py
@@ -33,12 +33,21 @@ def BasicBotView(request):
 @bot.callback_query_handler(func=lambda callback_query: True)
 def callback_inline(callback_query):
 
-    print("\n\n\n")
-    print("BUTTON")
-    print("\n\n\n")
+    if callback_query.data == "first":
+        button_text = "Зареєструватись"
+        button = telebot.types.InlineKeyboardButton(text=button_text, callback_data='register')
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row_width = 2
+        keyboard.add(button)
+        bot.send_message(message.chat.id, answer.call_for_registration_message, reply_markup=keyboard)
 
-    bot.send_message(callback_query.message.chat.id, callback_query.data)
-
+    if callback_query.data == "register":
+        button_text = "Ух!"
+        button = telebot.types.InlineKeyboardButton(text=button_text, callback_data='uh!')
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row_width = 2
+        keyboard.add(button)
+        bot.send_message(message.chat.id, "Зраз ми вас зареструемо!", reply_markup=keyboard)
 
 @bot.message_handler(commands=["letsfuck"])
 def handle_letsfuck_command(message):

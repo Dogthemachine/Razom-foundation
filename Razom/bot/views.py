6d5aa1ab9eeae3596d7e1bde814eed7c4c3f8877
Dogthemachine <danileyko@gmail.com>
2023-04-27 11:27:45 +0200

    Making dialog logic

--- Razom/bot/views.py
+++ Razom/bot/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect, get_object_or_404
-from bot.models import Recipients, Volunteers, Feedbacks, Requests, Categories, Address, Messages
+from bot.models import Recipients, Volunteers, Feedbacks, Requests, Categories, Address, Messages, Chata
 
 import environ
 import telebot
